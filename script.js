5b66858cd080d5c6ff1032d35bb4a1dcda809157
Hmorgan22 <huntermorgan1219@yahoo.com>
2024-01-13 16:40:28 -0500

    added loan method.

--- script.js
+++ script.js
@@ -167,3 +167,30 @@ btnTransfer.addEventListener('click', function (e) {
   inputTransferAmount.blur();
   updateUI(currentAccount);
 });
+
+btnLoan.addEventListener('click', function (e) {
+  e.preventDefault();
+  const amount = Number(inputLoanAmount.value);
+  if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
+    currentAccount.movements.push(amount);
+    updateUI(currentAccount);
+  }
+  inputLoanAmount.value = '';
+});
+
+btnClose.addEventListener('click', function (e) {
+  e.preventDefault();
+
+  if (
+    currentAccount.username === inputCloseUsername.value &&
+    currentAccount.pin === Number(inputClosePin.value)
+  ) {
+    const index = accounts.findIndex(
+      acc => acc.username === currentAccount.username
+    );
+    accounts.splice(index, 1);
+
+    inputCloseUsername.value = inputClosePin.value = '';
+    containerApp.style.opacity = 0;
+  }
+});
